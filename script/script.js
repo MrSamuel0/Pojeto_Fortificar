8102cd1838e6c913cd332d517f9a327d60f7ce77
Samuel Manhani Salles <samuca.manhani@gmail.com>
2024-06-01 19:17:02 -0300

    Atualizações
    adicionado: pagina de login completa (com js), alterações no index e no css.

---
+++ script/script.js
@@ -0,0 +1,25 @@
+function verifLogin() {
+    var login = "";
+    var senha = "";
+    login = document.getElementById('email').value;
+    senha = document.getElementById('senha').value;
+    if (login == "adm@uvv.br" && senha == "adm#123") {
+        let a = window.open("proponente.html");
+    }
+    else {
+        window.alert("Dados incorretos, tente novamente.");
+    }
+}
+
+function verifoLogin() {
+    var login = "";
+    var senha = "";
+    login = document.getElementById('ema').value;
+    senha = document.getElementById('sen').value;
+    if (login == "adm@uvv.br" && senha == "adm#123") {
+        let a = window.open("proponente.html");
+    }
+    else {
+        window.alert("Dados incorretos, tente novamente.");
+    }
+}
