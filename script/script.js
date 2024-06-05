0df617b205d2826f470e059d1f8126d5ece95c64
Samuel Manhani Salles <samuca.manhani@gmail.com>
2024-06-04 22:04:56 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,21 +4,13 @@ function verifLogin() {
     login = document.getElementById('email').value;
     senha = document.getElementById('senha').value;
     if (login == "adm@uvv.br" && senha == "adm#123") {
+        window.open("adm.html");
+    }
+        
+    else if (login == "adm@uvv.br" && senha == "adm#123") {
         window.open("proponente.html");
     }
-    else {
-        window.alert("Dados incorretos, tente novamente.");
-    }
-}
-
-function verifoLogin() {
-    var login = "";
-    var senha = "";
-    login = document.getElementById('ema').value;
-    senha = document.getElementById('sen').value;
-    if (login == "adm@uvv.br" && senha == "adm#123") {
-        window.open("proponente.html");
-    }
+    
     else {
         window.alert("Dados incorretos, tente novamente.");
     }
