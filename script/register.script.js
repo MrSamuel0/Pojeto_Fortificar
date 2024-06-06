c8dbb572db7e59bc80035e46b9777c78aa28dd54
Gabriel Puttin <gabrielputtin@hotmail.com>
2024-06-05 21:32:53 -0300

    Feat: Update register.script.js

--- script/register.script.js
+++ script/register.script.js
@@ -1,6 +1,6 @@
 function validateEqualPassword() {
     const passwords = document.getElementsByTagName("password");
-    if (passwords[0] === passwords[1]) {
+    if (passwords[0].value === passwords[1].value) {
         window.alert("As senhas precisam ser iguais")
     }
     return true;
